c5ad9e5c8f0c4a797ce94cd087fb0a8b0ac49393
haigbedros <haigbedros@gmail.com>
2018-02-07 13:41:51 -0800

    insalled module package and package.json file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Word = require("./word.js");
 module.exports = {
   //selects a random word from the array of words and returns it as a Word object
   selectWord : function() {
-    var words = ["Guatemala", "Bangladesh", "Indonesia", "Switzerland", "Australia"];
+    var words = ["California", "Utah", "Nevada", "Portland", "Arizona"];
     var num = Math.floor(Math.random() * 5);
     var word = new Word(words[num]);
     return word;
